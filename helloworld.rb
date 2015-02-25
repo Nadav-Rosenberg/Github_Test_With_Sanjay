ed4eff76ae78a9f1a476f8a0214448bf2dd6175c
sanjsanj <sanjsanj@hotmail.com>
2015-02-25 16:59:54 +0000

    Sanjay adds README and changes helloworld.rb

--- helloworld.rb
+++ helloworld.rb
@@ -4,3 +4,5 @@ puts "This is the fist version I have made on Nadav Mac"
 puts "Sanjay has received it and made changes, oh no!"
 
 puts "Nadav has accepted the changed on Github and now pulled it back to its local mac"
+
+puts "Sanjay has merged the fork again and made another change"
