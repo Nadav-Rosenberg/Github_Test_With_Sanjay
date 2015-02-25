be5fecbe2d5c8d0cfae32cb50b4c87d93729874b
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-02-25 16:47:56 +0000

    Adding another line on top of Sanjay change

--- helloworld.rb
+++ helloworld.rb
@@ -2,3 +2,5 @@ puts "Hello, World"
 puts "This is the fist version I have made on Nadav Mac"
 
 puts "Sanjay has received it and made changes, oh no!"
+
+puts "Nadav has accepted the changed on Github and now pulled it back to its local mac"
